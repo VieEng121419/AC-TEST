c0738b748d4b958b95d27677495dce6e56f18e3b
Anh Duong <anh.duong@apps-cyclone.com>
2024-01-05 14:05:47 +0700

    update

--- helper.js
+++ helper.js
@@ -1,4 +1,4 @@
 export const searchData = (data, keyword, value) => {
-    const results = data.filter((item) => item[keyword] === value);
+    const results = data.filter((item) => item[keyword].trim().toLowerCase() == value.trim().toLowerCase());
     return results;
   }
